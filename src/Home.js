0051b784a7025631807d8bfc52a13a0c9943074a
nishan-soni <nishan.soni2@gmail.com>
2021-08-13 00:27:58 -0600

    worked on homepage

--- src/Home.js
+++ src/Home.js
@@ -1,15 +1,34 @@
 import "./Home.css"
+import {motion} from 'framer-motion'
+import {useHistory, Link} from 'react-router-dom'
 
 const Home = () => {
+    let history = useHistory()
     return (
-        <div className = "container">
+        <motion.div
+            className = "container"
+            initial={{ y: -100 }}
+            animate={{ y: 0 }}
+            transition={{ type: "spring", stiffness: 50 }}
+         >
             <div className  ="title1">
                 Resume
             </div>
             <div className = "title2">
                 Creator
             </div>
-        </div>
+            <Link to='/creator' className = 'get-started-link'>
+                <motion.button
+                    className = 'get-started-btn'
+                    whileHover={{ scale: 1.1 }}
+                    whileTap={{ scale: 0.9 }}
+                    
+                >
+                    Get Started
+                </motion.button>
+            </Link>
+            
+        </motion.div>
         
     );
 }
