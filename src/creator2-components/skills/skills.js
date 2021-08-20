24e1747bfc22d5bc2f811f185d2d65ac846a44e1
nishan-soni <nishan.soni2@gmail.com>
2021-08-20 12:44:47 -0600

    added tabs

--- src/creator2-components/skills/skills.js
+++ src/creator2-components/skills/skills.js
@@ -1,10 +1,12 @@
 import './skills.css'
-
+import Tag from '../tag/tag';
 const Skills = () => {
     return (
         <div id = 'skills-container'>
             <div className = 'skills-title'>
                 Skills.
+            </div>
+            <div className = 'skill-tags'>
             </div>
         </div>
     );
---
+++ src/creator2-components/tag/tag.js
@@ -0,0 +1,21 @@
+import './tag.css'
+import DeleteIcon from '@material-ui/icons/Delete';
+import BackspaceIcon from '@material-ui/icons/Backspace';
+import HighlightOffIcon from '@material-ui/icons/HighlightOff';
+const Tag = (props) => {
+    
+    const {title} = props
+
+    return (
+        <div className = 'tag'>
+            <div style = {{paddingLeft : '0.5vw', paddingRight : '0.5vw', paddingTop : '0.25vw', paddingBottom : '0.25vw'}}>
+                {title}
+                <button className = 'delete-tag'>
+                    X
+                </button>
+            </div>
+        </div>
+    );
+}
+ 
+export default Tag;
