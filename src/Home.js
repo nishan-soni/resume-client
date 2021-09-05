455f582d6618181731a27eadc293e025994deb3d
Nishan Soni <nishan.soni2@gmail.com>
2021-09-04 22:35:11 -0600

    edited homepage title

--- src/Home.js
+++ src/Home.js
@@ -10,25 +10,26 @@ const Home = () => {
             animate={{ y: 0 }}
             transition={{ type: "spring", stiffness: 50 }}
          >
-            <div className  ="title1">
-                Resume
+            <div>
+                <div className  ="title1">
+                    Resume
+                </div>
+                <div className = "title2">
+                    Creator
+                </div>
+                <div style = {{width : 'fit-content', margin : 'auto'}}>
+                    <Link to='/creator' className = 'get-started-link'>
+                        <motion.button
+                            className = 'get-started-btn'
+                            whileHover={{ scale: 1.03 }}
+                            whileTap={{ scale: 0.9 }}
+                            
+                        >
+                            Get Started
+                        </motion.button>
+                    </Link>
+                </div>
             </div>
-            <div className = "title2">
-                Creator
-            </div>
-            <div style = {{width : 'fit-content', margin : 'auto'}}>
-                <Link to='/creator' className = 'get-started-link'>
-                    <motion.button
-                        className = 'get-started-btn'
-                        whileHover={{ scale: 1.03 }}
-                        whileTap={{ scale: 0.9 }}
-                        
-                    >
-                        Get Started
-                    </motion.button>
-                </Link>
-            </div>
-           
         </motion.div>
         
     );
