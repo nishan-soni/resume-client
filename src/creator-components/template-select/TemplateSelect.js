9367a9b1fc0709d081406a7a6a794a2b6fe51fad
Nishan Soni <nishan.soni2@gmail.com>
2021-09-09 23:24:25 -0600

    click outside to close window

--- src/creator-components/template-select/TemplateSelect.js
+++ src/creator-components/template-select/TemplateSelect.js
@@ -3,13 +3,29 @@ import { motion } from 'framer-motion';
 import Basic from '../images/basic.png'
 import Template1 from '../images/template1.png'
 import CloseIcon from '@material-ui/icons/Close';
+import { useEffect, useRef } from 'react';
 
 const TemplateSelect = (props) => {
 
+    const selectRef = useRef()
+
     const {setSelect, updatePointer, setColor} = props
 
+    useEffect(() => {
+        document.addEventListener("click", handleClickOutside, false);
+        return () => {
+          document.removeEventListener("click", handleClickOutside, false);
+        };
+      }, []);
+    
+      const handleClickOutside = event => {
+        if (selectRef.current && !selectRef.current.contains(event.target)) {
+          setSelect(false);
+        }
+      };
+
     return (
-        <motion.div initial = {{opacity : 0}} animate = {{opacity : 1}} transition = {{duration : 0.3}} className = 'template-container'>
+        <motion.div ref = {selectRef} initial = {{opacity : 0}} animate = {{opacity : 1}} transition = {{duration : 0.3}} className = 'template-container'>
             <div style = {{width : '100%', marginBottom : '2vh', paddingTop : '1vh'}}>
                 <button className = "close-templates" onClick= {() => {setSelect(false)}}>
                     <CloseIcon fontSize = 'large'/>
