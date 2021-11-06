6d14776bb4ac3bbf822d6110d2e1a02c51263969
Nishan Soni <nishan.soni2@gmail.com>
2021-11-06 17:33:32 -0600

    moved files around

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import {Route} from 'react-router-dom'
 import Home from './Home/Home';
-import Creator from './Creator';
+import Creator from './Creator/Creator';
 
 const App = () => {
     //{location.pathname === '/' ? null : <NavBar/>}
