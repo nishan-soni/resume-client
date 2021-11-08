a40f1e367bd109e23f5e4c20d7aac5b07c7f91f3
Nishan Soni <nishan.soni2@gmail.com>
2021-11-08 12:01:34 -0700

    Added signup section (not finished)

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import {Route} from 'react-router-dom'
 import Home from './Home/Home';
 import Creator from './Creator/Creator';
+import Signup from './Signup/Signup';
 
 const App = () => {
-    //{location.pathname === '/' ? null : <NavBar/>}
     return (
         <div>
             <Route exact path = '/' component = {Home}/>
             <Route exact path = '/creator' component = {Creator}/>
+            <Route exact path = '/signup' component = {Signup}/>
         </div>
     );
 }
